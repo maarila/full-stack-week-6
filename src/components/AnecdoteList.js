6f20c2f91a517c63b77f18ac8a8c9b99dc62d6d6
Mika Äärilä <maarila@gmail.com>
2018-01-11 17:28:05 +0200

    tehtävä 100

---
+++ src/components/AnecdoteList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {addingVote} from "../reducers/anecdoteReducer";
+
+class AnecdoteList extends React.Component {
+  render() {
+    const anecdotes = this.props.store.getState();
+    return (
+      <div>
+        <h2>Anecdotes</h2>
+        {anecdotes.sort((a, b) => b.votes - a.votes).map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button
+                onClick={() =>
+                  this.props.store.dispatch(addingVote(anecdote.id))
+                }>
+                vote
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default AnecdoteList;
