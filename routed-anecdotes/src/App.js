50967831a058e39bdb438e32c1d3e3fa117ca7e8
Mika Äärilä <maarila@gmail.com>
2018-01-13 11:00:59 +0200

    tehtävä 119

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Link, NavLink} from "react-router-dom";
+import {ListGroup, ListGroupItem} from "react-bootstrap";
 
 const Menu = () => (
   <div style={menuStyle}>
@@ -26,13 +27,13 @@ const activeStyle = {
 const AnecdoteList = ({anecdotes}) => (
   <div>
     <h2>Anecdotes</h2>
-    <ul>
+    <ListGroup>
       {anecdotes.map((anecdote) => (
-        <li key={anecdote.id}>
+        <ListGroupItem key={anecdote.id}>
           <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
-        </li>
+        </ListGroupItem>
       ))}
-    </ul>
+    </ListGroup>
   </div>
 );
 
@@ -235,7 +236,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <Router>
           <div>
             <div>
